373b09dcc990c8ee9203767f9bdfe605e022b062
nicolai <nicolaibah@gmail.com>
2021-03-25 20:59:29 +0100

    buggy fragment and nav added

--- StockzProjectApp/app/src/main/java/com/example/stockzprojectapp/MainActivity.kt
+++ StockzProjectApp/app/src/main/java/com/example/stockzprojectapp/MainActivity.kt
@@ -3,23 +3,51 @@ package com.example.stockzprojectapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), MarketAdapter.OnItemClickListener {
     private val dummylist = generateDummyList()
     private val adapter = MarketAdapter(dummylist, this)
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.market_rv)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        //setHasFixedSize is not neccesarry
-        recyclerView.setHasFixedSize(true)
-    }
+        val bnv: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bnv.setOnNavigationItemSelectedListener {
+            lateinit var selectedFragment: Fragment
+            when (it.itemId) {
+                R.id.portfolio -> {
+                    selectedFragment = PortfolioFragment()
+                }
+                R.id.search -> {
+                    selectedFragment = MarketFragment()
+                }
+                R.id.random -> {
+                    selectedFragment = RandomFragment()
+                }
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+            return@setOnNavigationItemSelectedListener true
+        }
+
+
+            val recyclerView: RecyclerView = findViewById(R.id.market_rv)
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            //setHasFixedSize is not neccesarry
+            recyclerView.setHasFixedSize(true)
+        }
+
+
+
+
+
 
     override fun onItemClick(position: Int) {
         Toast.makeText(this, "Item $position clicked", Toast.LENGTH_SHORT).show()
