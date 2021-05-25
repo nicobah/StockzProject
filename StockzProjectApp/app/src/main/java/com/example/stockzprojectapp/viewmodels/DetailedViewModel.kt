f5bda0290d2c7fdd9ecaa3bb39f2ce3fc860d730
nicolai <nicolaibah@gmail.com>
2021-05-25 11:19:12 +0200

    viewmodel created

---
+++ StockzProjectApp/app/src/main/java/com/example/stockzprojectapp/viewmodels/DetailedViewModel.kt
@@ -0,0 +1,83 @@
+package com.example.stockzprojectapp.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.stockzprojectapp.models.Stock
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import org.json.JSONObject
+import java.net.URL
+
+class DetailedViewModel: ViewModel() {
+
+    lateinit var dateKey : String
+    lateinit var json: JSONObject
+    private var stockPrice = MutableLiveData<String>()
+    private var percentRise = MutableLiveData<Float>()
+    private var todayHigh = MutableLiveData<Float>()
+    private var todayLow = MutableLiveData<Float>()
+    private var openingPrice = MutableLiveData<Float>()
+
+    fun getStockPrice(): LiveData<String> {
+        return stockPrice
+    }
+
+    fun getPercentRise(): LiveData<Float> {
+        return percentRise
+    }
+    fun getTodayHigh(): LiveData<Float>{
+        return todayHigh
+    }
+    fun getTodayLow(): LiveData<Float>{
+        return todayLow
+    }
+    fun getOpeningPrice(): LiveData<Float>{
+        return openingPrice
+    }
+
+
+
+
+    fun execute(symbol: String) {
+        val url5min = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=5min&apikey=KX1X7LVV24I06XJE".format(symbol)
+        val urlDaily = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + symbol + "&apikey=KX1X7LVV24I06XJE".format(symbol)
+        // send the URL request and read the response as a asyncrone call
+        println(symbol)
+
+        doAsync{
+            val result = URL(url5min).readText()
+            json = JSONObject(result)
+            dateKey = ((json["Meta Data"] as JSONObject).getString("3. Last Refreshed"))
+            var information = getInfo(json,"Time Series (5min)","1. open", result)
+
+            //var percentRise = ((informationDaily.toDouble() - information5min.toDouble())/informationDaily.toDouble())
+            if (information == "Invalid"){
+            } else {
+                println("Invaliud")
+                    stockPrice.postValue(information)
+            }
+            doAsync {
+                val result = URL(urlDaily).readText()
+                json = JSONObject(result)
+                dateKey = ((json["Meta Data"] as JSONObject).getString("3. Last Refreshed"))
+                var information = getInfo(json, "Time Series (Daily)", "4. close", result)
+                percentRise.postValue((information.toFloat()-getStockPrice().value!!.toFloat()) / information.toFloat()*100)
+                todayHigh.postValue( getInfo(json, "Time Series (Daily)", "2. high", result).toFloat())
+                todayLow.postValue(getInfo(json, "Time Series (Daily)", "3. low", result).toFloat())
+                openingPrice.postValue(getInfo(json, "Time Series (Daily)", "1. open", result).toFloat())
+            }
+        }
+    }
+
+
+    fun getInfo(json: JSONObject, timeFrame: String, name: String, text: String): String {
+        if (text.trim().length == 0 || ("Invalid" in text)) return "Invalid"
+        val message = text
+// get information from today
+        val information =
+            ((json[timeFrame] as JSONObject)[dateKey.toString()] as JSONObject).getString(name)
+        return information
+    }
+
+}
--- StockzProjectApp/app/src/main/java/com/example/stockzprojectapp/views/fragments/PortfolioDetailedFragment.kt
+++ StockzProjectApp/app/src/main/java/com/example/stockzprojectapp/views/fragments/PortfolioDetailedFragment.kt
@@ -6,23 +6,23 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.stockzprojectapp.viewmodels.PortfolioViewModel
 import com.example.stockzprojectapp.R
 import com.example.stockzprojectapp.databinding.FragmentDetailedPortfolioBinding
 import com.example.stockzprojectapp.models.Stock
+import com.example.stockzprojectapp.viewmodels.DetailedViewModel
+import com.example.stockzprojectapp.viewmodels.InspirationViewModel
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import org.json.JSONObject
 import java.net.URL
 
-class PortfolioDetailedFragment(symbol: String = "") : Fragment(R.layout.fragment_detailed_portfolio){
+class PortfolioDetailedFragment(symbol: String = "") :
+    Fragment(R.layout.fragment_detailed_portfolio) {
 
-    lateinit var dateKey : String
-    lateinit var json: JSONObject
+    private lateinit var viewModel: DetailedViewModel
     private var symbol: String = symbol
-
-    private val portfolioViewModel: PortfolioViewModel by activityViewModels()
-    //private lateinit var stockName: TextView
     private lateinit var stockValue: TextView
     private lateinit var todayRise: TextView
     private lateinit var todayHigh: TextView
@@ -33,6 +33,12 @@ class PortfolioDetailedFragment(symbol: String = "") : Fragment(R.layout.fragmen
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this).get(DetailedViewModel::class.java)
+        viewModel.execute(symbol)
+
+
+
+
         binding = FragmentDetailedPortfolioBinding.bind(view)
 
         stockValue = binding.stockPrice
@@ -41,68 +47,31 @@ class PortfolioDetailedFragment(symbol: String = "") : Fragment(R.layout.fragmen
         todayLow = binding.todayLow
         openingPrice = binding.openingPrice
 
+        viewModel.getStockPrice().observe(viewLifecycleOwner, Observer {
+            binding.stockPrice.setText(it)
+        })
+        viewModel.getPercentRise().observe(viewLifecycleOwner, Observer {
+            binding.todayRise.setText(it.toString())
+        })
+        viewModel.getTodayHigh().observe(viewLifecycleOwner, Observer {
+            binding.todayHigh.text = "Todays high: $it"
+        })
+        viewModel.getTodayLow().observe(viewLifecycleOwner, Observer {
+            binding.todayLow.text = "Todays low: $it"
+        })
+        viewModel.getOpeningPrice().observe(viewLifecycleOwner, Observer {
+            binding.openingPrice.text = "Opening price: $it"
+        })
+
         /*val portfolioObserver = Observer<Pair<Int, Stock>>{
             binding.stockSymbol.setText("${it.second.symbol}")
             execute("${it.second.symbol}")
         }*/
 
         binding.stockSymbol.setText("$symbol")
-        execute("$symbol")
 
         //portfolioViewModel.getSelectedStock().observe(viewLifecycleOwner, portfolioObserver)
     }
 
-    fun execute(symbol: String) {
-        val url5min = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=5min&apikey=KX1X7LVV24I06XJE".format(symbol)
-        val urlDaily = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + symbol + "&apikey=KX1X7LVV24I06XJE".format(symbol)
-        // send the URL request and read the response as a asyncrone call
-        println(symbol)
 
-        val fetch = doAsync{
-            val result = URL(url5min).readText()
-            json = JSONObject(result)
-            dateKey = ((json["Meta Data"] as JSONObject).getString("3. Last Refreshed"))
-            var information = getInfo(json,"Time Series (5min)","1. open", result)
-            //var percentRise = ((informationDaily.toDouble() - information5min.toDouble())/informationDaily.toDouble())
-            if (information == "Invalid"){
-            } else {
-                uiThread {
-                    //stockName.text = "Not possible"
-                    stockValue.text = "%.2f".format(information.toDouble())
-                }
-            }
-
-            val fetch = doAsync {
-                val result = URL(urlDaily).readText()
-                json = JSONObject(result)
-                dateKey = ((json["Meta Data"] as JSONObject).getString("3. Last Refreshed"))
-                var information = getInfo(json, "Time Series (Daily)", "4. close", result)
-                println(information)
-                println(stockValue.text)
-                var percentRise =
-                    ((information.toDouble()-stockValue.text.toString().toDouble()) / information.toDouble())
-                var high = getInfo(json, "Time Series (Daily)", "2. high", result).toDouble()
-                if (information == "Invalid") {
-                } else {
-                    uiThread {
-                        //stockName.text = "Not possible"
-                        todayRise.text = "%.2f".format(percentRise * 100) + "% - $" + "%.2f".format(stockValue.text.toString().toDouble() * percentRise)
-                        //var myString = myStock.symbol + System.getProperty("line.separator") + myStock.date + System.getProperty("line.separator")  +"$" + myStock.price
-                        todayLow.text = "Todays low: $" + "%.2f".format(getInfo(json, "Time Series (Daily)", "3. low", result).toDouble())
-                        todayHigh.text = "Todays high: $" + "%.2f".format(high)
-                        openingPrice.text = "Todays opening price: $" + "%.2f".format(getInfo(json, "Time Series (Daily)", "1. open", result).toDouble())
-                    }
-                }
-            }
-        }
-    }
-
-    fun getInfo(json: JSONObject, timeFrame: String, name: String, text: String): String {
-        if (text.trim().length == 0 || ("Invalid" in text)) return "Invalid"
-        val message = text
-// get information from today
-        val information =
-            ((json[timeFrame] as JSONObject)[dateKey.toString()] as JSONObject).getString(name)
-        return information
-    }
 }
